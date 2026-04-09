2944a7a00386942fcf318a639d37d17be2615db0
Paradise-ron <nikitakalyag@yandex.ru>
2026-04-09 13:21:30 +0300

    Добавлены задания Часть 1 и Часть 2

--- part1.php
+++ part1.php
@@ -3,7 +3,6 @@ echo "<h2>Часть 1: Ошибки и Исключения</h2>";
 
 // 1. Обработчик для открытия несуществующего файла
 $filename = 'non_existent_file.txt';
-// Используем оператор @ чтобы подавить стандартное предупреждение PHP и обработать его сами
 $handle = @fopen($filename, 'r');
 
 if ($handle === false) {
@@ -12,29 +11,26 @@ if ($handle === false) {
     fclose($handle);
 }
 
-// 2. Обработчик деления на ноль (сохранение в log.txt)
+// 2. Обработчик деления на ноль
 try {
     $a = 10;
     $b = 0;
     $result = $a / $b;
 } catch (DivisionByZeroError $e) {
-    // Записываем сообщение об ошибке в файл log.txt
-    file_put_contents('log.txt', "Ошибка деления на ноль: " . $e->getMessage() . PHP_EOL, FILE_APPEND);
-    echo "Произошла ошибка деления на ноль. Сообщение записано в log.txt.<br><br>";
+    file_put_contents('log.txt', "Ошибка: " . $e->getMessage() . PHP_EOL, FILE_APPEND);
+    echo "Произошла ошибка деления на ноль. Записано в log.txt<br><br>";
 }
 
-// 3. Обработчик для доступа к несуществующему элементу массива
+// 3. Обработчик для массива
 $countries = ['Spain' => 'Madrid', 'Russia' => 'Moscow'];
 $searchCountry = 'Germany';
 
 try {
-    // В PHP доступ к несуществующему ключу вызывает Warning, но не Exception.
-    // Чтобы использовать try-catch, мы должны сами создать проверку и выбросить исключение.
     if (!array_key_exists($searchCountry, $countries)) {
-        throw new Exception("Страна '$searchCountry' не найдена в массиве.");
+        throw new Exception("Страна '$searchCountry' не найдена.");
     }
     echo $countries[$searchCountry];
 } catch (Exception $e) {
-    echo "Обработка исключения: " . $e->getMessage() . "<br><br>";
+    echo "Обработка: " . $e->getMessage() . "<br><br>";
 }
 ?>
